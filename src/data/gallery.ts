1339a4d6247a69f00f1c3098f3db528cfecdb891
Jithu <abhinandca44@gmail.com>
2026-06-29 17:48:48 +0530

    Describe your changes

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -1,4 +1,5 @@
 import { GalleryItem } from "@/types";
 
 export const galleryItems: GalleryItem[] = [
+  { id: "gallery-1782735494013-oiiz", src: "/api/gallery/image/1782735494007-ixr8iz.webp", alt: "OIP", category: "flowers", width: 800, height: 600, type: "image" },
 ];
